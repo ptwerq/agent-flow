76431ac119353c3dbbeaada3ad5c02a8847384dd
«Taya <ptwerqtaya@gmail.com>
2026-07-25 13:52:54 +0300

    feat(client): add domain entities and basic configuration

---
+++ client-service/src/main/java/com/agentflow/clientservice/entity/DealStatus.java
@@ -0,0 +1,9 @@
+package com.agentflow.clientservice.entity;
+
+public enum DealStatus {
+    NEW,
+    IN_PROGRESS,
+    DOCS_PENDING,
+    SUCCESS,
+    CANCELLED
+}
---
+++ client-service/src/main/java/com/agentflow/clientservice/entity/ServiceType.java
@@ -0,0 +1,7 @@
+package com.agentflow.clientservice.entity;
+
+public enum ServiceType {
+    REAL_ESTATE,
+    INSURANCE,
+    LEGAL
+}
