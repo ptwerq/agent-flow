f2c429939944afbeca8a51cb93259274b91bb1c7
«Taya <ptwerqtaya@gmail.com>
2026-08-06 14:05:51 +0300

    feat: init manager-service project structure

---
+++ manager-service/src/main/java/com/agentflow/managerservice/ManagerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.agentflow.managerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class ManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerServiceApplication.class, args);
+    }
+
+}
